a05d5d4f033c7e7457f9cf61abd80e15fae208ce
Paweł Łaski <zniwus@gmail.com>
2014-04-18 18:12:06 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kwejk::Application.config.secret_key_base = 'b69665a96d248f9ff1fb17bfb2052c63e68d1bee0f6e4a837e47fcde5eb8ff41ca6f50ee47ef768dadae7728c2579e974993fef264ecb25ffebf2e8972f9cc4b'
