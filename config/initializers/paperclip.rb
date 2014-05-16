93dbfaafbfabf36a0180c5e7e7256632720dc150
Paweł Łaski <zniwus@gmail.com>
2014-05-16 14:26:11 +0200

    Add paperclip initializer

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,4 @@
+# config/initializers/paperclip.rb
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+
+#Paperclip::Attachment.default_options[:s3_protocol] = 'https'
