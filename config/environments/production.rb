116255c90783c55296eb665c7ceba4d6c7708927
Paweł Łaski <zniwus@gmail.com>
2014-05-16 15:15:56 +0200

    Next try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,9 +7,7 @@ Kwejk::Application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    :s3_permissions => :private,
-    :s3_host_name => 's3-eu-west-1.amazonaws.com',
-    :s3_protocol => 'https' 
+    :url => ":s3_eu_url"
   }
   # Settings specified here will take precedence over those in config/application.rb.
 
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,5 +2,7 @@
 #Paperclip::Attachment.default_options[:s3_host_name] = ENV['S3_ENDPOINT']
 
 #Paperclip::Attachment.default_options[:s3_protocol] = 'https'
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip.interpolates(:s3_eu_url) do |att, style| 
+"#{att.s3_protocol}://s3-website-eu-west-1.amazonaws.com/#{att.bucket_name}/#{att.path(style)}"
+end
