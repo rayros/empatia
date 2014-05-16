2a3124f7db8f0df0c1290979a030b96b2257e3c6
Paweł Łaski <zniwus@gmail.com>
2014-05-16 14:59:07 +0200

    Next try

--- app/models/post.rb
+++ app/models/post.rb
@@ -29,8 +29,6 @@ class Post < ActiveRecord::Base
   # For Paperclip pictures
   has_attached_file :picture,
     :styles => { :default => "610x>", :thumb => "100x100>" },
-    :url => "/:class/:id/:style/:basename.:extension"
-#    :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
   validates_attachment :picture,
     :presence => true,
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,3 +2,5 @@
 #Paperclip::Attachment.default_options[:s3_host_name] = ENV['S3_ENDPOINT']
 
 #Paperclip::Attachment.default_options[:s3_protocol] = 'https'
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
