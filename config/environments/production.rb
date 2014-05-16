fe967718e08c922be7c936119c65c8546733bf03
Paweł Łaski <zniwus@gmail.com>
2014-05-16 14:46:30 +0200

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Kwejk::Application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
     :s3_permissions => :private
   }
   # Settings specified here will take precedence over those in config/application.rb.
