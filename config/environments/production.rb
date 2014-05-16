66e20d773e30bc9d1fa6ff898c4523c6aa07faf4
Paweł Łaski <zniwus@gmail.com>
2014-05-16 16:38:15 +0200

    Fix "

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Kwejk::Application.configure do
     :bucket => "#{ENV['S3_BUCKET_NAME']}",
     :s3_credentials => {
       :access_key_id => "#{ENV['AWS_ACCESS_KEY_ID']}",
-      :secret_access_key => "#{ENV['AWS_SECRET_ACCESS_KEY']}
+      :secret_access_key => "#{ENV['AWS_SECRET_ACCESS_KEY']}"
     },
     :s3_permissions => :public_read,
     :s3_protocol => 'https',
