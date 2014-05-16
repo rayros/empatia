571f4d47d4bffe4537243621e4c28a5112df27fe
Paweł Łaski <zniwus@gmail.com>
2014-05-16 16:50:56 +0200

    Test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,10 +4,10 @@ Kwejk::Application.configure do
   # Paperclip config to amazon s3
   config.paperclip_defaults = {
     :storage => :s3,
-    :bucket => 'memes-demo',
+    :bucket => ENV['S3_BUCKET_NAME'],#'memes-demo',
     :s3_credentials => {
-      :access_key_id => 'AKIAJI3JTX5C5UJK6BJQ',
-      :secret_access_key => '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'
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],#'AKIAJI3JTX5C5UJK6BJQ',
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],#'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'
     },
     :s3_permissions => :public_read,
     :s3_protocol => 'https',
