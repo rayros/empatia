6a92c1ee5c44c7bd04c7244d662a0318f0557d2b
Paweł Łaski <zniwus@gmail.com>
2014-05-16 15:22:09 +0200

    Next try

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,7 +2,7 @@
 #Paperclip::Attachment.default_options[:s3_host_name] = ENV['S3_ENDPOINT']
 
 #Paperclip::Attachment.default_options[:s3_protocol] = 'https'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:path] = ':class/:attachment/:id_partition/:basename:style.:extension'
 Paperclip.interpolates(:s3_eu_url) do |att, style| 
 "#{att.s3_protocol}://s3-website-eu-west-1.amazonaws.com/#{att.bucket_name}/#{att.path(style)}"
 end
