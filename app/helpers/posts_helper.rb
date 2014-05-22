a67ef7d4767daca19ad66f8067377e7058267780
Paweł Łaski <zniwus@gmail.com>
2014-05-22 12:29:49 +0200

    Move conditions

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,11 @@
 module PostsHelper
   def waiting? post
-    (user_owner? post or admin_signed_in?) and post.accepted == false
+    user_owner? post and post.accepted == false
   end
 
   def user_owner? post
-    user_signed_in? and current_user.owner? post
+    (user_signed_in? and current_user.owner? post) or admin_signed_in?
   end
+
+
 end
