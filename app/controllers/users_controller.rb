aff8eb5c4c9350389a86dcc4a918742018090913
Paweł Łaski <zniwus@gmail.com>
2014-05-24 00:44:31 +0200

    Add comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,18 @@ class UsersController < ApplicationController
   expose(:user)
   expose(:users) { User.all.paginate(page: params[:page]) }
   expose(:posts) { Post.user_accepted(user).paginate(page: params[:page]) }
+  
+  # GET /users only for admins
   def index
   end
 
+  # GET /users/(id)
   def show
     self.posts = user.posts.order(:accepted).paginate(page: params[:page]) if admin_signed_in?
     self.posts = current_user.posts.order(:accepted).paginate(page: params[:page]) if user_signed_in?
   end
+
+  # DELETE /users/(id)
   def destroy
     user.destroy
     redirect_to users_path
