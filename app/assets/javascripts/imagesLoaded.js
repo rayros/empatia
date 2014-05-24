39ed0dba1298c1e6021d912141ea7de81f8749dc
Paweł Łaski <zniwus@gmail.com>
2014-05-24 04:28:27 +0200

    Add always

--- app/assets/javascripts/imagesLoaded.js
+++ app/assets/javascripts/imagesLoaded.js
@@ -1,9 +1,13 @@
 $(document).ready(function (){
-  $(".img-responsive").hide();
+  $('.img-responsive').hide();
   $('.post-url').addClass('loader');  
   var posts = document.querySelectorAll('.post');
   var imgLoad = imagesLoaded(posts);
-  imgLoad.on( 'progress', function( instance, image ) {
+  imgLoad.on('always', function(instance){
+      $('.post-url').removeClass('loader');
+      $(".img-responsive").show();
+  });
+  imgLoad.on('progress', function( instance, image ) {
       var elem = $(image.img);
       elem.show();
       elem.parent().removeClass('loader');
