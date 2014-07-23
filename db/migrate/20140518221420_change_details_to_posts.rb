56942798747c8628ade64e02483a005da3851599
Pawel Laski <laski@wit.edu.pl>
2014-07-23 18:32:05 -0400

    Fix update_all problem

--- db/migrate/20140518221420_change_details_to_posts.rb
+++ db/migrate/20140518221420_change_details_to_posts.rb
@@ -1,9 +1,9 @@
 class ChangeDetailsToPosts < ActiveRecord::Migration
   def change 
-    Post.update_all({comment_count: 0}, {comment_count: nil})
-    Post.update_all({like_count: 0}, {like_count: nil})
-    Post.update_all({share_count: 0}, {share_count: nil})
-    Post.update_all({total_count: 0}, {total_count: nil})
+    Post.where(comment_count: nil).update_all(comment_count: 0)
+    Post.where(like_count: nil).update_all(like_count: 0)
+    Post.where(share_count: nil).update_all(share_count: 0)
+    Post.where(total_count: nil).update_all(total_count: 0)
     change_column :posts, :comment_count, :integer, default: 0, null: false
     change_column :posts, :like_count, :integer, default: 0, null: false
     change_column :posts, :share_count, :integer, default: 0, null: false
