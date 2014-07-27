e98993f51b678ffc8b8103bfb031f5e110116389
Pawel Laski <laski@wit.edu.pl>
2014-07-27 15:36:32 -0400

    Change factories

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -1,13 +1,13 @@
 require 'factory_girl'
 
-def sample_file(filename = "sonic.png")
+def sample_file(filename: "sonic.png")
     File.new("app/assets/images/#{filename}")
 end
 
 FactoryGirl.define do
   factory :post do
-    title "Random"
-    description "MAMA"
+    title "Test picture."
+    description "Test picture."
     picture sample_file
   end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    email  "test@gmail.com"
-    password "password"
+    email                 { Faker::Internet.email }
+    password              "password"
     password_confirmation "password"  
   end
 end
