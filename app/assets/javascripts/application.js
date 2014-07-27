b16f829fa98ad6bfc5ebc802b3d3d620e3380b6b
Pawel Laski <laski@wit.edu.pl>
2014-07-27 19:50:40 -0400

    For bootstrap-sass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require bootstrap-sprockets
 //= require bootstrap-filestyle
 //= require_tree ./sitewide
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Kwejk::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -74,7 +74,7 @@ Kwejk::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( *.js *.css *.eot *.woff *.ttf *.svg )
+  #config.assets.precompile += %w( *.js *.css *.eot *.woff *.ttf *.svg )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
