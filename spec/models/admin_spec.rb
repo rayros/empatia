7a4fffecf75c3996dad849df6dd68f32c92cbc5e
Pawel Laski <laski@wit.edu.pl>
2014-08-09 13:25:28 -0400

    Add admin factory

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -27,5 +27,8 @@ require 'spec_helper'
 describe Admin do
   describe 'validations' do
     it { should validate_presence_of :email }
+    it 'has a valid factory' do
+      expect(create(:admin)).to be_valid
+    end
   end
 end
