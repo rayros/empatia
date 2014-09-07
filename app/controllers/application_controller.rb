74853e13645be08c948a734ba8733e6f39dc16c8
Paweł Łaski <master@master-VirtualBox.(none)>
2014-09-07 21:31:22 +0200

    remove layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  layout 'atheme'
   expose(:waiting_count) { Post.where(accepted: false).count }
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
